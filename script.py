93db533b82503cbc2278e474d3dadbd493fcfbae
sreyas05 <143534201+sreyas05@users.noreply.github.com>
2023-09-02 21:03:20 +0530

    Final task

--- script.py
+++
@@ -1,8 +0,0 @@
-import os
-
-x=str(input("Enter X: "))
-y=str(input("Enter Y: "))
-
-file_name = os.popen(f"cat {x}/Spell_{y}.txt ").read().split('\n')[0]
-os.system(f"cp spellbook/{file_name}.py codes/")
-os.system(f"python3 spellbook/{file_name}.py")
